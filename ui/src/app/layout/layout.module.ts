301faa2d53f79cb7ac0bfe2c18b0c48f6da7fea3
Pedro Arenas <pedroars99@icloud.com>
2023-11-26 00:42:59 +0100

    Display app icon on the navbar

--- ui/src/app/layout/layout.module.ts
+++ ui/src/app/layout/layout.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { ThemeTogglerComponent } from './theme-toggler/theme-toggler.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [NavbarComponent, ThemeTogglerComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, SharedModule],
   exports: [NavbarComponent],
 })
 export class LayoutModule {}
