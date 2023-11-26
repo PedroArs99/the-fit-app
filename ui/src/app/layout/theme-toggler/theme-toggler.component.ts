342045fdddce7c6d0bf0c5e3cca1cde591d9471a
Pedro Arenas <pedroars99@icloud.com>
2023-11-26 12:26:22 +0100

    Auto detect system theme

--- ui/src/app/layout/theme-toggler/theme-toggler.component.ts
+++ ui/src/app/layout/theme-toggler/theme-toggler.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'tfa-theme-toggler',
   templateUrl: './theme-toggler.component.html',
-  styleUrl: './theme-toggler.component.scss'
+  styleUrl: './theme-toggler.component.scss',
 })
-export class ThemeTogglerComponent {
+export class ThemeTogglerComponent implements OnInit {
+  prefersLightSchema = false;
 
+  ngOnInit(): void {
+    const prefersLightSchemaQuery = window.matchMedia('(prefers-color-scheme: light)');
+
+    this.prefersLightSchema = prefersLightSchemaQuery.matches;
+  }
 }
--- ui/src/app/shared/icons.config.ts
+++ ui/src/app/shared/icons.config.ts
@@ -1,3 +1,4 @@
 import { faDatabase } from '@fortawesome/free-solid-svg-icons';
+import { faMoon, faSun } from '@fortawesome/free-regular-svg-icons';
 
-export const icons = [faDatabase];
+export const icons = [faDatabase, faMoon, faSun];
