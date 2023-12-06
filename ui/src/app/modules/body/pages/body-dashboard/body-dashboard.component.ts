85a188b39e5c09730f77ad5ac7c841c04bed514c
Pedro Arenas <pedroars99@icloud.com>
2023-12-06 18:17:05 +0100

    Display real data points

--- ui/src/app/modules/body/pages/body-dashboard/body-dashboard.component.ts
+++ ui/src/app/modules/body/pages/body-dashboard/body-dashboard.component.ts
@@ -11,13 +11,20 @@ export class BodyDashboardComponent {
     {
       timestamp: new Date(2023, 10, 28),
       weight: 74.1,
-      muscleMassPercentage: 0.81,
-      fatMassPercentage: 0.147,
-      boneMassPercentage: 0.043,
+      muscleMassPercentage: 81,
+      fatMassPercentage: 14.7,
+      boneMassPercentage: 4.3,
+    },
+    {
+      timestamp: new Date(2023, 11, 4),
+      weight: 74.3,
+      muscleMassPercentage: 81.5,
+      fatMassPercentage: 14.2,
+      boneMassPercentage: 4.2,
     },
   ]);
 
-  weightTimeSeries = computed(() => this.metrics().map((it) => ({ timestamp: it.timestamp, value: it.weight })));
+  weightTimeSeries = computed(() => this.metrics().map((it) => ({ timestamp: new Date(it.timestamp), value: it.weight })));
   muscleMassTimeSeries = computed(() =>
     this.metrics().map((it) => ({ timestamp: it.timestamp, value: it.muscleMassPercentage }))
   );
--- ui/src/app/shared/components/charts/line-chart/line-chart.component.ts
+++ ui/src/app/shared/components/charts/line-chart/line-chart.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgChartsModule } from 'ng2-charts';
 import { ChartConfiguration, ChartOptions } from 'chart.js';
+import { TimeSerie } from '../../../models/time-serie';
 
 @Component({
   selector: 'tfa-line-chart',
@@ -10,24 +11,37 @@ import { ChartConfiguration, ChartOptions } from 'chart.js';
   templateUrl: './line-chart.component.html',
   styleUrl: './line-chart.component.scss',
 })
-export class LineChartComponent {
-  lineChartData: ChartConfiguration<'line'>['data'] = {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-    datasets: [
-      {
-        data: [65, 59, 80, 81, 56, 55, 40],
-        label: 'Series A',
-        fill: true,
-        tension: 0.5,
-        borderColor: 'black',
-        backgroundColor: 'rgba(255,0,0,0.3)',
-      },
-    ],
-  };
+export class LineChartComponent implements OnInit {
+  @Input({ required: true }) timeSerie!: TimeSerie;
+
+  lineChartData = signal<ChartConfiguration<'line'>['data'] | undefined>(undefined);
 
   lineChartOptions: ChartOptions<'line'> = {
-    responsive: false,
+    responsive: true,
+    plugins: {
+      legend: {
+        display: false,
+      },
+    },
   };
 
-  lineChartLegend = true;
+  ngOnInit(): void {
+    const labels = this.timeSerie.map((entry) => new Date(entry.timestamp).toDateString());
+    const data = this.timeSerie.map((entry) => entry.value);
+
+    this.lineChartData.set({
+      labels,
+      datasets: [
+        {
+          data,
+          fill: true,
+          tension: 0.5,
+          borderColor: '#ffdb1d',
+          backgroundColor: '#ffdb1d',
+          pointBackgroundColor: '#000000',
+          pointBorderColor: '#000000',
+        },
+      ],
+    });
+  }
 }
