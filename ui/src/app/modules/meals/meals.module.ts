002ff190f2e5ed1c243871d0e5af1c00e8248c7b
Pedro Arenas <pedroars99@icloud.com>
2024-01-20 20:54:27 +0100

    Lazy load meals and reorder dashboard

--- ui/src/app/modules/meals/meals.module.ts
+++ ui/src/app/modules/meals/meals.module.ts
@@ -3,14 +3,24 @@ import { CommonModule } from '@angular/common';
 import { MealsComponent } from './pages/meals/meals.component';
 import { MealCardComponent } from './components/meal-card/meal-card.component';
 import { SharedModule } from '../../shared/shared.module';
-import { MealDetailsComponent } from './components/meal-details/meal-details.component';
+import { MealDetailsComponent } from './pages/meal-details/meal-details.component';
 import { MealsRepository } from './services/meals-repository.service';
 import { RouterModule, Routes } from '@angular/router';
+import { mealByIdResolver } from './resolvers/meal-by-id.resolver';
 
 const routes: Routes = [
   {
     path: '',
     component: MealsComponent,
+  },
+  {
+    path: ':id',
+    component: MealDetailsComponent,
+    resolve: mealByIdResolver,
+  },
+  {
+    path: '**',
+    redirectTo: '',
   },
 ];
 @NgModule({
