07dfd039bbc8ae475a879b30e86785a6fcdaede8
Pedro Arenas <pedroars99@icloud.com>
2024-01-20 20:30:41 +0100

    Change themes

--- ui/src/app/layout/layout.module.ts
+++ ui/src/app/layout/layout.module.ts
@@ -1,11 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
-import { ThemeTogglerComponent } from './theme-toggler/theme-toggler.component';
 import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [NavbarComponent, ThemeTogglerComponent],
+  declarations: [NavbarComponent],
   imports: [CommonModule, SharedModule],
   exports: [NavbarComponent],
 })
--- ui/tailwind.config.js
+++ ui/tailwind.config.js
@@ -2,15 +2,39 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   daisyui: {
-    themes: ["sunset", "bumblebee"],
+    themes: [
+      {
+        mytheme: {
+          primary: "#a900ff",
+
+          secondary: "#ff2b00",
+
+          accent: "#0074bf",
+
+          neutral: "#150702",
+
+          "base-100": "#fbfbff",
+
+          info: "#00e8ff",
+
+          success: "#00c68e",
+
+          warning: "#ecc200",
+
+          error: "#dd0045",
+        },
+      },
+      "cupcake",
+      "sunset",
+      "bumblebee",
+    ],
   },
   theme: {
     extend: {
       spacing: {
-        '112': '27rem'
-      }
+        112: "27rem",
+      },
     },
   },
   plugins: [require("daisyui")],
 };
-
