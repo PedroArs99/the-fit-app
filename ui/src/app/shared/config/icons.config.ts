d67234fb03cbdf5560f8a5e5c7c670f692913408
Pedro Arenas <pedroars99@icloud.com>
2024-01-25 20:51:53 +0100

    Reorder navbar

--- ui/src/app/shared/config/icons.config.ts
+++ ui/src/app/shared/config/icons.config.ts
@@ -1,4 +1,4 @@
-import { faChevronDown, faChildReaching, faDatabase, faUtensils } from '@fortawesome/free-solid-svg-icons';
+import { faBookOpen, faChevronDown, faChildReaching, faDatabase, faUtensils } from '@fortawesome/free-solid-svg-icons';
 import { faMoon, faSun } from '@fortawesome/free-regular-svg-icons';
 
-export const icons = [faChevronDown, faChildReaching, faDatabase, faMoon, faSun, faUtensils];
+export const icons = [faBookOpen, faChevronDown, faChildReaching, faDatabase, faMoon, faSun, faUtensils];
