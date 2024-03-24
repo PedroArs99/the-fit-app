d209b12b25eae5b9f8b0f350a0f4526cac0e82ba
Pedro Arenas <pedroars99@icloud.com>
2024-03-24 20:47:57 +0100

    Add training plan details page

--- ui/src/app/modules/training-plans/training-plans.module.ts
+++ ui/src/app/modules/training-plans/training-plans.module.ts
@@ -5,11 +5,18 @@ import { RouterModule, Routes } from '@angular/router';
 import { SharedModule } from '@shared/shared.module';
 import { TrainingPlansRepository } from './services/training-plans-repository.service';
 import { TrainingPlanCardComponent } from './components/training-plan-card/training-plan-card.component';
+import { TrainingPlanDetailsPageComponent } from './pages/training-plan-details/training-plan-details-page.component';
+import { trainingPlanByIdResolver } from './resolvers/training-plan-by-id.resolver';
 
 const routes: Routes = [
   {
     path: '',
     component: TrainingPlansPageComponent,
+  },
+  {
+    path: ':id',
+    component: TrainingPlanDetailsPageComponent,
+    resolve: { trainingPlan: trainingPlanByIdResolver },
   },
 ];
 
