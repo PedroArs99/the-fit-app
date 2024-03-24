67dd55a78393174d957c6acc43c149e7afca7761
Pedro Arenas <pedroars99@icloud.com>
2024-03-24 19:48:02 +0100

    Create toCamelCase Pipe

--- ui/src/app/shared/shared.module.ts
+++ ui/src/app/shared/shared.module.ts
@@ -5,11 +5,12 @@ import { icons } from './config/icons.config';
 import { SpinnerComponent } from './components/spinner/spinner.component';
 import { RouterModule } from '@angular/router';
 import { ToasterComponent } from './components/toaster/toaster.component';
+import { ToCamelCasePipe } from './pipes/to-camel-case.pipe';
 
 @NgModule({
-  declarations: [SpinnerComponent, ToasterComponent],
+  declarations: [SpinnerComponent, ToasterComponent, ToCamelCasePipe],
   imports: [CommonModule, FontAwesomeModule, RouterModule],
-  exports: [FontAwesomeModule, RouterModule, SpinnerComponent, ToasterComponent],
+  exports: [FontAwesomeModule, RouterModule, SpinnerComponent, ToasterComponent, ToCamelCasePipe],
 })
 export class SharedModule {
   constructor(iconLibrary: FaIconLibrary) {
