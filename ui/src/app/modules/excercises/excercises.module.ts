0bc2a340fa62f56efde96d3c5d9ccf89395b24f5
Pedro Arenas <par@novatec-gmbh.de>
2024-04-13 23:47:39 +0200

    Cleanup Excercises UI

--- ui/src/app/modules/excercises/excercises.module.ts
+++ ui/src/app/modules/excercises/excercises.module.ts
@@ -5,22 +5,17 @@ import { ExcercisesPageComponent } from './pages/excercises/excercises.component
 import { ExcerciseCardComponent } from './components/excercise-card/excercise-card.component';
 import { SharedModule } from '@shared/shared.module';
 import { ExcercisesRepository } from './services/excercises-repository.service';
-import { ExcerciseUpsertPageComponent } from './pages/excercise-upsert-page/excercise-upsert.component';
 import { ReactiveFormsModule } from '@angular/forms';
 
 const routes: Routes = [
   {
     path: '',
     component: ExcercisesPageComponent,
-  },
-  {
-    path: 'new',
-    component: ExcerciseUpsertPageComponent,
-  },
+  }
 ];
 
 @NgModule({
-  declarations: [ExcerciseCardComponent, ExcercisesPageComponent, ExcerciseUpsertPageComponent],
+  declarations: [ExcerciseCardComponent, ExcercisesPageComponent],
   imports: [CommonModule, ReactiveFormsModule, RouterModule.forChild(routes), SharedModule],
   providers: [ExcercisesRepository],
 })
--- ui/src/app/modules/excercises/services/excercises-repository.service.ts
+++ ui/src/app/modules/excercises/services/excercises-repository.service.ts
@@ -3,22 +3,10 @@ import { Signalizable } from '@shared/models/signalizable.model';
 import { environment } from '@environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { Excercise } from '../models/excercise.model';
-import { Observable, map } from 'rxjs';
-
-type AddExcerciseInput = {
-  name: string;
-  imageUrl: string;
-};
 
 @Injectable()
 export class ExcercisesRepository {
   constructor(private httpClient: HttpClient) {}
-
-  add(input: AddExcerciseInput, callback?: (excercise: Excercise) => void): Observable<Excercise> {
-    return this.httpClient
-      .post<Excercise>(`${environment.apiUrl}/excercises`, input)
-      .pipe(map((response) => this.mapResponseEntryToDomain(response)));
-  }
 
   getAll(): Signalizable<Excercise[]> {
     const isLoading = signal(true);
