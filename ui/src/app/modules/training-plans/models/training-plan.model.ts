0912679c7da2a6966ecd00c42bc2964dcaf49c9c
Pedro Arenas <par@novatec-gmbh.de>
2024-04-29 22:13:23 +0200

    Fetch series and reps from the backend

--- ui/src/app/modules/training-plans/models/training-plan.model.ts
+++ ui/src/app/modules/training-plans/models/training-plan.model.ts
@@ -1,9 +1,13 @@
 import { Excercise } from '../../excercises/models/excercise.model';
 
-interface Workout {
-  name: string,
-  excercises: Excercise[],
-}
+type Workout = {
+  name: string;
+  excercises: {
+    series: number;
+    reps: number;
+    excercise: Excercise;
+  }[];
+};
 
 export class TrainingPlan {
   constructor(public id: number, public name: string, public workouts: Workout[]) {}
