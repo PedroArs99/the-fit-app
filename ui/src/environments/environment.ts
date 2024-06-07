057ec584bc9a29bea6a82794966b687299b420df
Pedro Arenas <par@novatec-gmbh.de>
2024-06-07 19:26:26 +0200

    Change api url for prod

--- ui/src/environments/environment.ts
+++ ui/src/environments/environment.ts
@@ -1,5 +1,5 @@
 import { Environment } from './environment.model';
 
 export const environment: Environment = {
-  apiUrl: 'https://lsa33kbplj.execute-api.eu-central-1.amazonaws.com',
+  apiUrl: 'https://1u1ui33ru9.execute-api.eu-central-1.amazonaws.com',
 };
