bdff9d36e262ced7e477c58022ac0b95611a9a86
Pedro Arenas <par@novatec-gmbh.de>
2024-06-07 19:49:09 +0200

    Group Exercises by category

--- ui/src/app/modules/excercises/pages/excercises/excercises.component.ts
+++ ui/src/app/modules/excercises/pages/excercises/excercises.component.ts
@@ -1,13 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, computed } from '@angular/core';
 import { ExcercisesRepository } from '../../services/excercises-repository.service';
+import { Exercise } from '../../models/exercise.model';
 
 @Component({
   selector: 'tfa-excercises',
   templateUrl: './excercises.component.html',
-  styleUrl: './excercises.component.scss'
+  styleUrl: './excercises.component.scss',
 })
 export class ExcercisesPageComponent {
-  excercises = this.excercisesRepository.getAll();
+  private _excercises = this.excercisesRepository.getAll();
 
-  constructor(private excercisesRepository: ExcercisesRepository){}
-}
+  isLoading = this._excercises.isLoading;
+  
+  exercisesByCategory = computed(() => {
+    const exercises = this._excercises.value(); 
+    return this.groupExercisesByCategory(exercises);
+  });
+
+  constructor(private excercisesRepository: ExcercisesRepository) {}
+
+  private groupExercisesByCategory(exercises: Exercise[]): { [key: string]: Exercise[] } {
+    return exercises.reduce((acc, item) => {
+      const categoryExercises = acc[item.category];
+      if (!categoryExercises) {
+        acc[item.category] = [item];
+      } else {
+        acc[item.category] = [...categoryExercises, item];
+      }
+
+      return acc;
+    }, {} as { [key: string]: Exercise[] });
+  }
+} 
