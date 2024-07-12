fe58f28997ea4ecc897d89b2d5d832f0fda9478b
Pedro Arenas <par@novatec-gmbh.de>
2024-07-13 01:38:39 +0200

    Filter exercises out by category

--- ui/src/app/modules/excercises/models/exercise.model.ts
+++ ui/src/app/modules/excercises/models/exercise.model.ts
@@ -8,8 +8,18 @@ export type Exercise = {
   id: number;
   name: string;
   description: string;
-  category: string;
+  category: Category;
   equipment: Array<string>;
   image: string;
   diary: Array<DiaryEntry>;
 };
+
+export enum Category {
+  ABS = 'Abs',
+  ARMS = 'Arms',
+  BACK = 'Back',
+  CALVES = 'Calves',
+  CHEST = 'Chest',
+  LEGS = 'Legs',
+  SHOULDERS = 'Shoulders',
+}
