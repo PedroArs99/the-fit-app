a3ccdaf80b281a793c26b6d2364b28b8413a0ba1
Pedro Arenas <pedroars99@icloud.com>
2024-08-20 19:22:40 +0200

    Add exercises.findById

--- src/lib/exercises/exercise.repository.ts
+++ src/lib/exercises/exercise.repository.ts
@@ -1,16 +1,28 @@
-import type { Exercise } from "$lib/exercises/exercise.model";
-import { supabase } from "$lib/supabase/client";
+import type { Exercise } from '$lib/exercises/exercise.model';
+import { supabase } from '$lib/supabase/client';
 
 class ExerciseRepository {
-  private tableName = 'exercises';
+	private tableName = 'exercises';
 
-  async readAll(): Promise<Exercise[]> {
-    const { data, error } = await supabase.from(this.tableName).select('*');
+	async findAll(): Promise<Exercise[]> {
+		const { data, error } = await supabase.from(this.tableName).select('*');
 
+		if (error) throw Error(error.message);
+
+		return data;
+	}
+
+	async findById(id: string): Promise<Exercise | null> {
+    const { data, error } = await supabase.from(this.tableName).select('*').eq('id', id);
+    
     if (error) throw Error(error.message);
 
-    return data;
-  }
+    if (data) {
+      return data[0];
+    } else {
+      return null;
+    }
+	}
 }
 
 export const exerciseRepository = new ExerciseRepository();
