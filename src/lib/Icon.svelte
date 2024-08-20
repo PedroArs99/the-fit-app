db30f51602c814f0c625fd9a332948af5a4d7d07
Pedro Arenas <pedroars99@icloud.com>
2024-08-21 00:26:49 +0200

    Add trash icon

--- src/lib/Icon.svelte
+++ src/lib/Icon.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
-	import { faArrowUpRightFromSquare, faBars, faDumbbell, faPlus } from '@fortawesome/free-solid-svg-icons';
+	import { faArrowUpRightFromSquare, faBars, faDumbbell, faPlus, faTrash } from '@fortawesome/free-solid-svg-icons';
 	import Fa from 'svelte-fa';
 
-	export let icon: 'bars' | 'dumbbell' | 'link-new-tab' | 'plus';
+	export let icon: 'bars' | 'dumbbell' | 'link-new-tab' | 'plus' | 'trash';
 
 	const iconToDefinition = {
 		bars: faBars,
 		dumbbell: faDumbbell,
 		'link-new-tab': faArrowUpRightFromSquare,
 		'plus': faPlus,
+		'trash': faTrash,
 	};
 </script>
 
