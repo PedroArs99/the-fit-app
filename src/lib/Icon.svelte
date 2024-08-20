204823f6b4dd46df72ef501a5ac0fa0ab801add9
Pedro Arenas <pedroars99@icloud.com>
2024-08-21 00:07:49 +0200

    Add plus icon

--- src/lib/Icon.svelte
+++ src/lib/Icon.svelte
@@ -1,13 +1,14 @@
 <script lang="ts">
-	import { faArrowUpRightFromSquare, faBars, faDumbbell } from '@fortawesome/free-solid-svg-icons';
+	import { faArrowUpRightFromSquare, faBars, faDumbbell, faPlus } from '@fortawesome/free-solid-svg-icons';
 	import Fa from 'svelte-fa';
 
-	export let icon: 'bars' | 'dumbbell' | 'link-new-tab';
+	export let icon: 'bars' | 'dumbbell' | 'link-new-tab' | 'plus';
 
 	const iconToDefinition = {
 		bars: faBars,
 		dumbbell: faDumbbell,
-		'link-new-tab': faArrowUpRightFromSquare
+		'link-new-tab': faArrowUpRightFromSquare,
+		'plus': faPlus,
 	};
 </script>
 
