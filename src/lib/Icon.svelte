43ac9145d87df8a6e6c63563e63b47a76a7c50cb
Pedro Arenas <pedroars99@icloud.com>
2024-08-20 19:04:20 +0200

    Add link-new-tab icon

--- src/lib/Icon.svelte
+++ src/lib/Icon.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
-	import { faBars, faDumbbell, type IconDefinition } from '@fortawesome/free-solid-svg-icons';
+	import { faArrowUpRightFromSquare, faBars, faDumbbell } from '@fortawesome/free-solid-svg-icons';
 	import Fa from 'svelte-fa';
 
-	export let icon: 'bars' | 'dumbbell';
+	export let icon: 'bars' | 'dumbbell' | 'link-new-tab';
 
 	const iconToDefinition = {
 		bars: faBars,
 		dumbbell: faDumbbell,
+		'link-new-tab': faArrowUpRightFromSquare
 	};
 </script>
 
