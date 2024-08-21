16543479a84359e233a43f19957e29328e922716
Pedro Arenas <pedroars99@icloud.com>
2024-08-21 16:42:58 +0200

    Add pencil icon

--- src/lib/Icon.svelte
+++ src/lib/Icon.svelte
@@ -1,14 +1,15 @@
 <script lang="ts">
-	import { faArrowUpRightFromSquare, faBars, faDumbbell, faPlus, faTrash } from '@fortawesome/free-solid-svg-icons';
+	import { faArrowUpRightFromSquare, faBars, faDumbbell, faPencil, faPlus, faTrash } from '@fortawesome/free-solid-svg-icons';
 	import Fa, { type IconSize } from 'svelte-fa';
 
-	export let icon: 'bars' | 'dumbbell' | 'link-new-tab' | 'plus' | 'trash';
+	export let icon: 'bars' | 'dumbbell' | 'link-new-tab' | 'pencil' | 'plus' | 'trash';
 	export let size: IconSize | undefined = undefined;
 
 	const iconToDefinition = {
 		bars: faBars,
 		dumbbell: faDumbbell,
 		'link-new-tab': faArrowUpRightFromSquare,
+		'pencil': faPencil,
 		'plus': faPlus,
 		'trash': faTrash,
 	};
