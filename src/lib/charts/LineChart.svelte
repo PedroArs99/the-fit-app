8bc3d048aa66c8bd2798482732f3c26f4c4197e1
Pedro Arenas <pedroars99@icloud.com>
2024-08-21 17:52:49 +0200

    Add Line Chart component

---
+++ src/lib/charts/LineChart.svelte
@@ -0,0 +1,43 @@
+<script lang="ts">
+	import { Chart, LineController, LineElement } from 'chart.js';
+	import { onMount } from 'svelte';
+
+  Chart.register(
+    LineController,
+    LineElement,
+  )
+
+	export let dataPoints: { label: string, value: number}[];
+
+	onMount(() => {
+		const ctx = document.getElementById('line-chart') as HTMLCanvasElement;
+
+		if (ctx) {
+			new Chart(ctx, {
+				type: 'line',
+				data: {
+					labels: dataPoints.map((it) => it.label),
+					datasets: [
+						{
+							data: dataPoints.map((it) => it.value),
+              backgroundColor: 'rgb(255,219,1)',
+              borderColor: 'rgb(255,219,1)',
+              tension: 0.25,
+						}
+					],
+				},
+        options: {
+          plugins: {
+            legend: {
+              display: false,
+            }
+          },
+        }
+      },);
+		}
+	});
+</script>
+
+<div class="p-1 md:p-3">
+	<canvas id="line-chart" class="w-full"></canvas>
+</div>
