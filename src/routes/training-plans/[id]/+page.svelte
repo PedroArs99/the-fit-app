b025ef502685e5c1266336a66e50a8a944198032
Pedro Arenas <pedroars99@icloud.com>
2024-08-25 19:33:08 +0200

    Add training plan details page

---
+++ src/routes/training-plans/[id]/+page.svelte
@@ -0,0 +1,15 @@
+<script lang="ts">
+	import LineChart from '$lib/charts/LineChart.svelte';
+	import type { TrainingPlan } from '$lib/training-plans/training-plan.model';
+
+	export let data: { trainingPlan: TrainingPlan };
+
+</script>
+
+<div class="training-plan">
+	<h1>{data.trainingPlan.name}</h1>
+</div>
+
+<style lang="postcss">
+	
+</style>
