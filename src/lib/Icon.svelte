5e48182687d99eb6b2910fb448a576ae750ec29f
Pedro Arenas <pedroars99@icloud.com>
2024-08-25 20:00:28 +0200

    Add chevron icons

--- src/lib/Icon.svelte
+++ src/lib/Icon.svelte
@@ -1,19 +1,40 @@
 <script lang="ts">
-	import { faArrowUpRightFromSquare, faBars, faDumbbell, faList, faPencil, faPlus, faTrash } from '@fortawesome/free-solid-svg-icons';
+	import {
+		faArrowUpRightFromSquare,
+		faBars,
+		faChevronLeft,
+		faChevronRight,
+		faDumbbell,
+		faList,
+		faPencil,
+		faPlus,
+		faTrash
+	} from '@fortawesome/free-solid-svg-icons';
 	import Fa, { type IconSize } from 'svelte-fa';
 
-	export let icon: 'bars' | 'dumbbell' | 'link-new-tab' | 'list' | 'pencil' | 'plus' | 'trash';
+	export let icon:
+		| 'bars'
+		| 'chevron-left'
+		| 'chevron-right'
+		| 'dumbbell'
+		| 'link-new-tab'
+		| 'list'
+		| 'pencil'
+		| 'plus'
+		| 'trash';
 	export let size: IconSize | undefined = undefined;
 
 	const iconToDefinition = {
 		bars: faBars,
+		'chevron-left': faChevronLeft,
+		'chevron-right': faChevronRight,
 		dumbbell: faDumbbell,
 		'link-new-tab': faArrowUpRightFromSquare,
-		'list': faList,
-		'pencil': faPencil,
-		'plus': faPlus,
-		'trash': faTrash,
+		list: faList,
+		pencil: faPencil,
+		plus: faPlus,
+		trash: faTrash
 	};
 </script>
 
-<Fa icon={iconToDefinition[icon]} size={size} />
+<Fa icon={iconToDefinition[icon]} {size} />
