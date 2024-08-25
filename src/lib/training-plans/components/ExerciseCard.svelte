857c294876902f9abca80cf26cb8cb9f0fb80870
Pedro Arenas <pedroars99@icloud.com>
2024-08-25 20:38:54 +0200

    Display exercises on training plans

---
+++ src/lib/training-plans/components/ExerciseCard.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+	import Icon from '$lib/Icon.svelte';
+	import { createEventDispatcher } from 'svelte';
+	import type { Exercise } from '$lib/exercises/exercise.model';
+	import { fade } from 'svelte/transition';
+
+	export let exercise: Exercise;
+
+	const dispatch = createEventDispatcher();
+</script>
+
+<div out:fade class="card md:max-w-56">
+	<div class="card-actions">
+		<a href={`/exercises/${exercise.id}`} class="btn btn-sm btn-ghost">
+			<Icon icon="link-new-tab" />
+		</a>
+	</div>
+
+	<figure>
+		{#if exercise.image}
+			<img src={exercise.image} alt={exercise.name} />
+		{:else}
+			<Icon icon="dumbbell" size="3x" />
+		{/if}
+	</figure>
+
+	<div class="card-body">
+		<h2 class="card-title">{exercise.name}</h2>
+	</div>
+</div>
+
+<style lang="postcss">
+	.card-actions {
+		display: flex;
+		flex-direction: row-reverse;
+
+		@apply p-1 gap-1;
+	}
+</style>
--- src/routes/training-plans/[id]/+page.svelte
+++ src/routes/training-plans/[id]/+page.svelte
@@ -1,11 +1,12 @@
 <script lang="ts">
 	import Icon from '$lib/Icon.svelte';
-	import type { TrainingPlan } from '$lib/training-plans/training-plan.model';
+	import ExerciseCard from '$lib/training-plans/components/ExerciseCard.svelte';
+	import type { TrainingPlan, Workout } from '$lib/training-plans/training-plan.model';
 
 	export let data: { trainingPlan: TrainingPlan };
 	let activeWorkoutIdx: number = 0;
 
-	$: activeWorkout = data.trainingPlan.workouts[activeWorkoutIdx];
+	$: activeWorkout = data.trainingPlan.workouts[activeWorkoutIdx] as Workout;
 </script>
 
 <div class="training-plan">
@@ -21,20 +22,37 @@
 				<Icon icon="chevron-right" />
 			</button>
 		</div>
+
+		<div class="exercises">
+			{#each activeWorkout.exercises as exercise}
+				<ExerciseCard exercise={exercise} />
+			{/each}
+		</div>
 	</div>
 </div>
 
 <style lang="postcss">
-	.workout-switcher {
+	.active-workout {
 		display: flex;
-		flex-direction: row;
-		justify-content: space-between;
+		flex-direction: column;
+
+		@apply gap-3;
+	}
+
+	.exercises {
+		@apply flex flex-col md:flex-row gap-3;
 	}
 
 	.training-plan {
 		display: flex;
 		flex-direction: column;
-
+		
 		@apply gap-3;
+	}
+	
+	.workout-switcher {
+		display: flex;
+		flex-direction: row;
+		justify-content: space-between;
 	}
 </style>
