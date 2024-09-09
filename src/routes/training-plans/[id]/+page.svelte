a0dc9995a7e5766e63939329d6eb3b3646bf09d0
Pedro Arenas <par@novatec-gmbh.de>
2024-09-09 23:42:49 +0200

    Add navigation Arrows to the training Plans

--- src/routes/training-plans/[id]/+page.svelte
+++ src/routes/training-plans/[id]/+page.svelte
@@ -1,12 +1,40 @@
 <script lang="ts">
+	import { browser } from '$app/environment';
 	import Icon from '$lib/Icon.svelte';
 	import ExerciseCard from '$lib/training-plans/components/ExerciseCard.svelte';
 	import type { TrainingPlan, Workout } from '$lib/training-plans/training-plan.model';
 
-	export let data: { trainingPlan: TrainingPlan };
-	let activeWorkoutIdx: number = 0;
+	export let data: { activeWorkoutIdx: number; trainingPlan: TrainingPlan };
 
-	$: activeWorkout = data.trainingPlan.workouts[activeWorkoutIdx] as Workout;
+	function nextWorkout() {
+		if (data.activeWorkoutIdx + 1 < workouts.length) {
+			data.activeWorkoutIdx += 1;
+		}
+	}
+
+	function previousWorkout() {
+		if (data.activeWorkoutIdx > 0) {
+			data.activeWorkoutIdx -= 1;
+		}
+	}
+
+	function synchronizeSearchParams(activeWorkout: string) {
+		if(browser) {
+			// Create a new URLSearchParams object from the current URL
+			let params = new URLSearchParams(window.location.search);
+	
+			// Add or update the query parameters
+			params.set('activeWorkout', activeWorkout);
+	
+			// Update the browser's URL without reloading the page
+			const newUrl = `${window.location.pathname}?${params.toString()}`;
+			window.history.pushState({}, '', newUrl);
+		}
+	}
+
+	$: workouts = data.trainingPlan.workouts;
+	$: activeWorkout = data.trainingPlan.workouts[data.activeWorkoutIdx] as Workout;
+	$: synchronizeSearchParams(`${data.activeWorkoutIdx}`);
 </script>
 
 <div class="training-plan">
@@ -14,18 +42,26 @@
 
 	<div class="active-workout">
 		<div class="workout-switcher">
-			<button class="btn btn-ghost btn-outline btn-disabled">
+			<button
+				class="btn btn-ghost btn-outline"
+				disabled={data.activeWorkoutIdx === 0}
+				on:click={previousWorkout}
+			>
 				<Icon icon="chevron-left" />
 			</button>
 			<h2>{activeWorkout.name}</h2>
-			<button class="btn btn-ghost btn-outline btn-disabled">
+			<button
+				class="btn btn-ghost btn-outline"
+				disabled={data.activeWorkoutIdx === workouts.length - 1}
+				on:click={nextWorkout}
+			>
 				<Icon icon="chevron-right" />
 			</button>
 		</div>
 
 		<div class="exercises">
 			{#each activeWorkout.exercises as exercise}
-				<ExerciseCard exercise={exercise} />
+				<ExerciseCard {exercise} />
 			{/each}
 		</div>
 	</div>
@@ -46,10 +82,10 @@
 	.training-plan {
 		display: flex;
 		flex-direction: column;
-		
+
 		@apply gap-3;
 	}
-	
+
 	.workout-switcher {
 		display: flex;
 		flex-direction: row;
--- src/routes/training-plans/[id]/+page.ts
+++ src/routes/training-plans/[id]/+page.ts
@@ -2,14 +2,16 @@ import type { PageLoad } from './$types';
 import { error } from '@sveltejs/kit';
 import { trainingPlanRepository } from '$lib/training-plans/training-plan.repository';
 
-export const load: PageLoad = async ({ params }) => {
+export const load: PageLoad = async ({ params, url }) => {
 	try {
 		const id = params.id;
 		const trainingPlan = await trainingPlanRepository.findById(id);
 
 		if (!trainingPlan) error(404, `Training plan with id ${id} was not found.`);
 
-		return { trainingPlan };
+		const activeWorkoutIdx = url.searchParams.get('activeWorkout') ?? 0;
+
+		return { activeWorkoutIdx, trainingPlan };
 	} catch (e) {
 		error(500, (e as Error).message);
 	}
