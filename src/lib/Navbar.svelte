a451cd0f87bdce4dbc5670f7b4465b1e13ca69ae
Pedro Arenas <par@novatec-gmbh.de>
2024-10-23 20:51:03 +0200

    Remove navbar

--- src/lib/Navbar.svelte
+++ src/lib/Navbar.svelte
@@ -4,35 +4,9 @@
 
 <div class="navbar">
 	<a href="/" class="text-lg font-bold">The Fit App</a>
-
-	<div class="dropdown dropdown-end">
-		<div tabindex="0" role="button" class="btn btn-ghost rounded-btn">
-			<Icon icon="bars" />
-		</div>
-		<ul class="menu dropdown-content">
-			<li>
-				<a href="/exercises">
-					<Icon icon="dumbbell" />
-					<span>Exercises</span>
-				</a>
-			</li>
-			<li>
-				<a href="/training-plans">
-					<Icon icon="list" />
-					<span>Training Plans</span>
-				</a>
-			</li>
-		</ul>
-	</div>
 </div>
 
 <style lang="postcss">
-	.dropdown-content {
-		width: calc(100vw - 1rem);
-
-		@apply mt-4 p-2 z-[1] rounded-box shadow-2xl bg-base-100;
-	}
-
 	.navbar {
 		display: flex;
 		justify-content: space-between;
