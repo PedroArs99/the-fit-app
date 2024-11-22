3b65f5476d72bb4ab0a7fe6c1693bc7faed37337
Pedro Arenas <par@novatec-gmbh.de>
2024-11-22 23:45:44 +0100

    Change Theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ import daisyui from 'daisyui';
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	daisyui: {
-		themes: ['bumblebee']
+		themes: [
+			{
+				tfa: {
+					primary: '#e07a5f',
+					'primary-content': '#3d405b',
+
+					secondary: '#3d405b',
+					accent: '#81b29a',
+					neutral: '#f2cc8f',
+
+					'base-100': '#f4f1de',
+					'base-content': '#3d405b',
+
+					'--rounded-btn': '2rem'
+				}
+			}
+		]
 	},
 	theme: {
 		extend: {}
