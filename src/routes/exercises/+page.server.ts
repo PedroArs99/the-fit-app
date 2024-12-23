ce8287be67ff30073af566366437a0c68e74c38c
Pedro Arenas <par@novatec-gmbh.de>
2024-12-24 00:26:44 +0100

    Add Categories as /exercise page data

--- src/routes/exercises/+page.server.ts
+++ src/routes/exercises/+page.server.ts
@@ -4,8 +4,11 @@ import { exerciseRepository } from '$lib/exercises/exercise.repository';
 export const load = async () => {
 	try {
 		const exercises = await exerciseRepository.findAll();
+		const categories = new Set(
+			exercises.map((it) => it.category).sort((a, b) => a.localeCompare(b))
+		);
 
-		return { exercises };
+		return { categories, exercises };
 	} catch (e) {
 		error(500, (e as Error).message);
 	}
