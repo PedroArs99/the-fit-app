ecdc0d98a9a0dedba5fbb4ec87bd319e89015c16
Pedro Arenas <par@novatec-gmbh.de>
2025-02-02 20:57:05 +0100

    Fix workout Navigation

--- src/routes/training-plans/[id]/+page.svelte
+++ src/routes/training-plans/[id]/+page.svelte
@@ -1,58 +1,55 @@
 <script lang="ts">
-	import { run } from 'svelte/legacy';
-
-	import { browser } from '$app/environment';
 	import Icon from '$lib/ui/components/Icon.svelte';
 	import ExerciseCard from '$lib/training-plans/components/ExerciseCard.svelte';
-	import type { TrainingPlan, Workout } from '$lib/training-plans/training-plan.model';
+	import type { TrainingPlan } from '$lib/training-plans/training-plan.model';
+	import { pushState } from '$app/navigation';
 
 	interface Props {
 		data: { activeWorkoutIdx: number; trainingPlan: TrainingPlan };
 	}
 
-	let { data = $bindable() }: Props = $props();
+	let props: Props = $props();
+	
+	let trainingPlan = props.data.trainingPlan;
+	let workouts = trainingPlan.workouts;
+
+	let activeWorkoutIdx = $state(props.data.activeWorkoutIdx);
+	let activeWorkout = $derived(workouts[activeWorkoutIdx]);
 
 	function nextWorkout() {
-		if (data.activeWorkoutIdx + 1 < workouts.length) {
-			data.activeWorkoutIdx += 1;
+		if (activeWorkoutIdx + 1 < workouts.length) {
+			activeWorkoutIdx += 1;
+			synchronizeSearchParams(activeWorkoutIdx)
 		}
 	}
 
 	function previousWorkout() {
-		if (data.activeWorkoutIdx > 0) {
-			data.activeWorkoutIdx -= 1;
+		if (activeWorkoutIdx > 0) {
+			activeWorkoutIdx -= 1;
 		}
 	}
 
-	function synchronizeSearchParams(activeWorkout: string) {
-		if (browser) {
+	function synchronizeSearchParams(activeWorkoutIdx: number) {
 			// Create a new URLSearchParams object from the current URL
 			let params = new URLSearchParams(window.location.search);
 
 			// Add or update the query parameters
-			params.set('activeWorkout', Number.parseInt(activeWorkout, 10).toString());
+			params.set('activeWorkout', activeWorkoutIdx.toString());
 
 			// Update the browser's URL without reloading the page
 			const newUrl = `${window.location.pathname}?${params.toString()}`;
-			window.history.pushState({}, '', newUrl);
-		}
+			pushState(newUrl, {});
 	}
-
-	let workouts = $derived(data.trainingPlan.workouts);
-	let activeWorkout = $derived(data.trainingPlan.workouts[data.activeWorkoutIdx] as Workout);
-	run(() => {
-		synchronizeSearchParams(`${data.activeWorkoutIdx}`);
-	});
 </script>
 
 <div class="training-plan">
-	<h1>{data.trainingPlan.name}</h1>
+	<h1>{trainingPlan.name}</h1>
 
 	<div class="active-workout">
 		<div class="workout-switcher">
 			<button
 				class="btn btn-ghost btn-outline"
-				disabled={data.activeWorkoutIdx === 0}
+				disabled={activeWorkoutIdx === 0}
 				onclick={previousWorkout}
 			>
 				<Icon icon="chevron-left" />
@@ -60,7 +57,7 @@
 			<h2>{activeWorkout.name}</h2>
 			<button
 				class="btn btn-ghost btn-outline"
-				disabled={data.activeWorkoutIdx === workouts.length - 1}
+				disabled={activeWorkoutIdx === workouts.length - 1}
 				onclick={nextWorkout}
 			>
 				<Icon icon="chevron-right" />
