a5a17f4ec2e3222dbeafbb39272d6b257747c919
Pedro Arenas <par@novatec-gmbh.de>
2025-02-13 23:11:03 +0100

    Create Page Component

---
+++ src/lib/ui/components/Page.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import type { SvelteComponent } from 'svelte';
+
+	interface Props {
+		children: SvelteComponent;
+		title: string;
+	}
+
+	let { children, title } = $props();
+</script>
+
+<div class="page">
+	<h1>{title}</h1>
+
+	{@render children()}
+</div>
+
+<style lang="postcss">
+	.page {
+		display: flex;
+		flex-direction: column;
+
+		@apply gap-3;
+	}
+</style>
