8abf7a320eefc2b09bebf48a8e239aa68f35f4ad
Pedro Arenas <par@novatec-gmbh.de>
2025-03-09 22:04:56 +0100

    List shoes

---
+++ ui/src/routes/running/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	import FullWidthCard from '$lib/ui/components/cards/FullWidthCard.svelte';
+	import Page from '$lib/ui/components/Page.svelte';
+	import type { Shoe } from '../../lib/running/shoes/shoe.model';
+
+	interface Props {
+		data: { shoes: Shoe[] };
+	}
+
+	let { data }: Props = $props();
+	let shoes = data.shoes;
+</script>
+
+<Page title="Shoes">
+		{#each shoes as shoe}
+			<FullWidthCard>
+				<div class="flex justify-start">
+					<img
+						class="card-image"
+						width="256px"
+						height="256px"
+						src={shoe.imageUrl}
+						alt={shoe.name}
+					/>
+
+					<span class="card-title">{shoe.name}</span>
+				</div>
+			</FullWidthCard>
+		{/each}
+</Page>
---
+++ ui/src/routes/running/+page.ts
@@ -0,0 +1,13 @@
+import type { PageLoad } from './$types';
+import { error } from '@sveltejs/kit';
+import { shoeRepository } from '../../lib/running/shoes/shoe.repository';
+
+export const load: PageLoad = async () => {
+	try {
+		const shoes = await shoeRepository.findAll();
+
+		return { shoes };
+	} catch (e) {
+		error(500, (e as Error).message);
+	}
+};
