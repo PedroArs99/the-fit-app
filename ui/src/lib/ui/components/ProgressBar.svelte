974ccc0af081823e6176cb147fcb9db5d6ae15f0
Pedro Arenas <par@novatec-gmbh.de>
2025-04-05 23:14:50 +0200

    Create ProgressBar

---
+++ ui/src/lib/ui/components/ProgressBar.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  type Props = {
+    label?: string;
+    max: number;
+    value: number;
+  }
+
+  let {value, max, label }: Props = $props();
+</script>
+
+<progress class="progress progress-primary" value={value} max={max} data-label={label}></progress>
+
+<style>
+  progress {
+    height: 1.5em;
+  }
+
+	progress::before {
+		content: attr(data-label);
+    font-weight: 0.8em;
+    text-align: center;
+    vertical-align: middle;
+
+		/*Position text over the progress bar */
+		position: absolute;
+    /* top: 5px; */
+		left: 0;
+		right: 0;
+	}
+</style>
