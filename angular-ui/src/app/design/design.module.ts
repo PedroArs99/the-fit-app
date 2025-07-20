bd06633d08b2aa7dcc9cfbd5a4706715e0da8a15
Pedro Arenas <pedroars99@icloud.com>
2025-07-20 19:24:33 +0200

    Add Css to the page title

---
+++ angular-ui/src/app/design/design.module.ts
@@ -0,0 +1,17 @@
+import { Input, NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { PageTitleComponent } from './page-title/page-title.component';
+
+
+@NgModule({
+  declarations: [
+    PageTitleComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    PageTitleComponent
+  ]
+})
+export class DesignModule { }
