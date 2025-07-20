0ef310cbbd6cba1e43ce22ee6c7359bb94b7975a
Pedro Arenas <pedroars99@icloud.com>
2025-07-20 20:44:00 +0200

    Create Nav Menu component

--- angular-ui/src/app/design/nav-menu/nav-menu.component.ts
+++ angular-ui/src/app/design/nav-menu/nav-menu.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { SizeProp } from '@fortawesome/angular-fontawesome';
+import { IconsModule } from '../icons.module';
 
 @Component({
     selector: 'app-nav-menu',
     templateUrl: './nav-menu.component.html',
-    standalone: false,
+    imports: [
+        RouterModule,
+        IconsModule,
+    ],
     styleUrl: './nav-menu.component.css',
 })
 export class NavMenuComponent {
-
+    iconSize: SizeProp = "2xl"
 }
