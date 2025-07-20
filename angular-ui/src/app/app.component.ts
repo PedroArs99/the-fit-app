45ca5d3fdca8d1ddff10c993df9433445f8274cc
Pedro Arenas <pedroars99@icloud.com>
2025-07-20 20:44:14 +0200

    Display Nav Menu and adjust primary color

--- angular-ui/src/app/app.component.ts
+++ angular-ui/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { DesignModule } from './design/design.module';
+import { NavMenuComponent } from './design/nav-menu/nav-menu.component';
 
 @Component({
     selector: 'app-root',
     imports: [
         DesignModule,
+        NavMenuComponent,
         RouterOutlet
     ],
     templateUrl: './app.component.html',
