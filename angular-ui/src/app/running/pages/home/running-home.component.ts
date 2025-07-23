ab2ffaf1e5f2379b5e6fed06648d6d8754880b11
Pedro Arenas <pedroars99@icloud.com>
2025-07-23 21:38:41 +0200

    Move running home page

--- angular-ui/src/app/running/pages/home/running-home.component.ts
+++ angular-ui/src/app/running/pages/home/running-home.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { DesignModule } from "../../../design/design.module";
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-running-home',
-  imports: [DesignModule],
+  imports: [
+    DesignModule,
+    RouterModule
+  ],
   templateUrl: './running-home.component.html',
   styleUrl: './running-home.component.css'
 })
