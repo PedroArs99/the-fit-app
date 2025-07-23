e1dcc0e2300426623f9a7e6fad61c623bac4c1a1
Pedro Arenas <pedroars99@icloud.com>
2025-07-23 21:27:28 +0200

    Add section-header component

--- angular-ui/src/app/design/design.module.ts
+++ angular-ui/src/app/design/design.module.ts
@@ -2,18 +2,23 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PageTitleComponent } from './page-title/page-title.component';
 import { IconsModule } from './icons.module';
+import { SectionHeaderComponent } from './section-header/section-header.component';
 
+const components = [
+  PageTitleComponent,
+  SectionHeaderComponent,
+]
 
 @NgModule({
   declarations: [
-    PageTitleComponent,
+    ...components
   ],
   imports: [
     CommonModule,
     IconsModule,
   ],
   exports: [
-    PageTitleComponent,
+    ...components
   ]
 })
 export class DesignModule { }
