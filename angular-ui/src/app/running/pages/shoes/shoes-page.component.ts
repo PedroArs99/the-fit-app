a778f3a0d1d18a587fb61665ac16d5d551ac5382
Pedro Arenas <pedroars99@icloud.com>
2025-07-23 21:38:12 +0200

    Add shoes page

---
+++ angular-ui/src/app/running/pages/shoes/shoes-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { DesignModule } from "../../../design/design.module";
+
+@Component({
+  selector: 'app-shoes-page',
+  imports: [DesignModule],
+  templateUrl: './shoes-page.component.html',
+  styleUrl: './shoes-page.component.css'
+})
+export class ShoesPageComponent {
+
+}
--- angular-ui/src/app/running/running-routing.module.ts
+++ angular-ui/src/app/running/running-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Route, RouterModule } from '@angular/router';
-import { RunningHomeComponent } from './pages/running-home/running-home.component';
+import { RunningHomeComponent } from './pages/home/running-home.component';
+import { ShoesPageComponent } from './pages/shoes/shoes-page.component';
 
 const routes: Route[] = [
-  { path: '', component: RunningHomeComponent }
+  { path: '', component: RunningHomeComponent },
+  { path: 'shoes', component: ShoesPageComponent }
 ]
 
 @NgModule({
