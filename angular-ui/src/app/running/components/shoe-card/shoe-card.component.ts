fe4db02878370db53c9d88decd5a11afe65f3fdb
Pedro Arenas <pedroars99@icloud.com>
2025-07-23 21:51:48 +0200

    Create shoe card component

---
+++ angular-ui/src/app/running/components/shoe-card/shoe-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-shoe-card',
+  standalone: false,
+  templateUrl: './shoe-card.component.html',
+  styleUrl: './shoe-card.component.css'
+})
+export class ShoeCardComponent {
+
+}
--- angular-ui/src/app/running/running.module.ts
+++ angular-ui/src/app/running/running.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RunningRoutingModule } from './running-routing.module';
-
-
+import { ShoeCardComponent } from './components/shoe-card/shoe-card.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ShoeCardComponent
+  ],
   imports: [
     CommonModule,
     RunningRoutingModule
